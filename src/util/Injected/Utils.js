8fac5e9e2ebec9f6a9aabf80145e08ec119b8663
dgstx <dgsomacal@gmail.com>
2026-01-20 08:30:35 -0300

    fix: Adjust `sendSeen` arguments in `markChatRead` based on WhatsApp Web version.

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -13,10 +13,16 @@ exports.LoadUtils = () => {
         const chat = await window.WWebJS.getChat(chatId, { getAsModel: false });
         if (chat) {
             window.Store.WAWebStreamModel.Stream.markAvailable();
-            await window.Store.SendSeen.sendSeen({
-                chat: chat,
-                threadId: undefined
-            });
+
+            if (window.compareWwebVersions(window.Debug.VERSION, '<=', '2.3000.1031980585')) {
+                await window.Store.SendSeen.sendSeen(chat);
+            } else {
+                await window.Store.SendSeen.sendSeen({
+                    chat: chat,
+                    threadId: undefined
+                });
+            }
+
             window.Store.WAWebStreamModel.Stream.markUnavailable();
             return true;
         }
